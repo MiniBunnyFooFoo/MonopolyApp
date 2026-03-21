842f0e0985122c08cf2063cef4ca9f35a24ac5cc
MiniBunnyFooFoo <98247220+MiniBunnyFooFoo@users.noreply.github.com>
2026-03-22 02:58:07 +1100

    corrected formatting errors with css and style tags

--- frontend/styles.py
+++ frontend/styles.py
@@ -4,4 +4,4 @@ def get_css():
     log_box = load_css("log_box.css")
     board = load_css("board.css")
     p_card = load_css("player_card.css")
-    return board + log_box + p_card
+    return "<style>" + board + p_card + log_box + "</style>"
